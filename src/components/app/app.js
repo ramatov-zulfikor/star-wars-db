4f8880d6c9543b666f552524d28430df32626f80
ramatov-zulfikor <ramatovv@gmail.com>
2021-01-11 16:02:25 +0500

    base structure for react-router

---
+++ src/components/app/app.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react'
+
+import Header from '../header'
+import RandomPlanet from '../random-planet'
+import ErrorBoundary from '../error-boundary'
+import SwapiService from '../../services/swapi-service'
+import DummySwapiService from '../../services/dummy-swapi-service'
+
+import { PeoplePage, PlanetsPage, StarshipsPage } from '../pages'
+import { SwapiServiceProvider } from '../swapi-service-context'
+
+import './app.css'
+export default class App extends Component {
+
+  state = {
+    swapiService: new SwapiService()
+  }
+
+  onServiceChange = () => {
+    this.setState(({ swapiService }) => {
+      const Service = swapiService instanceof SwapiService ?
+        DummySwapiService : SwapiService
+      return {
+        swapiService: new Service()
+      }
+    })
+  }
+
+  render() {
+
+    return (
+      <ErrorBoundary>
+        <SwapiServiceProvider value={this.state.swapiService} >
+          <div className="stardb-app">
+            <Header onServiceChange={this.onServiceChange} />
+
+            <RandomPlanet />
+            <PeoplePage />
+            <PlanetsPage />
+            <StarshipsPage />
+
+          </div>
+        </SwapiServiceProvider>
+      </ErrorBoundary>
+    )
+  }
+}
